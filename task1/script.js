dae8264d4e493656752e22a1f2b84cac150ea9f4
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2023-09-08 09:35:52 +0100

    Update script.js

--- task1/script.js
+++ task1/script.js
@@ -2,7 +2,7 @@ const currentDayOfTheWeek = document.getElementById("currentDayOfTheWeek")
 const sec = document.getElementById("currentUTCTime")
 console.log(sec);
 
-
+const date = new Date()
 const day = date.getDay()
 const daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 const time = date.getTime()
